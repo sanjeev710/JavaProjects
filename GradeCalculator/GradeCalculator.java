cb3b538344645e1d1a0e5b73fd21021f221af8eb
Sanjeev V A <sanjeevva2005@gmail.com>
2026-08-17 11:33:53 +0530

    changes made

---
+++ GradeCalculator/GradeCalculator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class GradeCalculator{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int marks = scan.nextInt();
+        System.out.println(marks);
+    }
+}
